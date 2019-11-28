944935b80072c4a9e4052ec27b4b046cc74f5e4b
idward <idward@vip.sina.com>
2019-11-28 22:25:36 +0800

    update files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { RadioGroup } from 'uxcore';
+
+const { Item } = RadioGroup;
 
 class App extends React.Component {
-  render(){
+  handleChange = (e: any) => {
+    console.log('changed: ', e);
+  };
+
+  render() {
     return (
       <div>
         <h1>Welcome to My React Boilerplate!</h1>
+        <RadioGroup value="apple" onChange={this.handleChange}>
+          <Item value="apple" text="苹果" />
+          <Item value="banana" text="香蕉" />
+          <Item value="orange" text="橘子" />
+        </RadioGroup>
       </div>
-    )
+    );
   }
 }
 
